b24bb71477e1fd8ae90e3c834fa0990fd6fa7eb3
Nilanjan Deb <nilanjan172nsvian@gmail.com>
2020-06-21 21:39:40 +0530

    add query

--- README.md
+++ README.md
@@ -117,11 +117,11 @@ Type User {
 * Automation & Hinting
 
 
-#### Learning Plan ( v1 )
+### Learning Plan ( v1 )
 
-* Build the back end server
-* Setup Express With Express-Graphql
-* Create schema file with queries and mutations
-* Implement JSON-Server
-* CRUD Functionality
-* Test with Graphiql
+1. Build the back end server
+2. Setup Express With Express-Graphql
+3. Create schema file with queries and mutations
+4. Implement JSON-Server
+5. CRUD Functionality
+6. Test with Graphiql
--- v1/schema.js
+++ v1/schema.js
@@ -6,21 +6,87 @@ const {
     GraphQLList,
     GraphQLNonNull
 } = require('graphql');
+const axios = require('axios');
+const config = {
+    baseURL: 'http://localhost:3000'
+}
+
+const fetchCustomer = async (query='') => {
+    const res = await axios.get(`customers/${query}`, config);
+    return res.data
+}
+
+
+// // hardcoded Data
+// const customers = [
+//     {id: "1", name: 'nilanjan1', email: 'nil1@gmail.com', age: 20},
+//     {id: "2", name: 'nilanjan2', email: 'nil2@gmail.com', age: 19},
+//     {id: "3", name: 'nilanjan3', email: 'nil3@gmail.com', age: 18}
+// ];
+
 
 // Customer Type
 const CustomerType = new GraphQLObjectType({
     name: 'Customer',
     fields: () => ({
         id: {type: GraphQLString},
-        name: {type: Gra}
+        name: {type: GraphQLString},
+        email: {type: GraphQLString},
+        age: {type: GraphQLInt}
     })
-}) 
+}); 
 
 // Root Query
 const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    customer: {
-        type: CustomerType
+    name: 'MyFirstQuery',
+    fields: {
+        customer: {
+            type: CustomerType,
+            args: {
+                id:{type: GraphQLString}
+            },
+            resolve(parentvalue, args) {
+                return fetchCustomer(args.id)
+            }
+        },
+        customers: {
+            type: new GraphQLList(CustomerType),
+            resolve(parentvalue, args){
+                return fetchCustomer()
+            }
+        }
+    }
+});
+// Mutation Type
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addCustomer: {
+            type: CustomerType,
+            args: {
+                name: {type: new GraphQLNonNull(GraphQLString)},
+                email: {type: new GraphQLNonNull(GraphQLString)},
+                age: {type: new GraphQLNonNull(GraphQLInt)}
+            },
+            resolve(parentvalue, args){
+                return axios.post('/customers', {
+                    name: args.name,
+                    email: args.email,
+                    age: args.age
+                }, config)
+                .then(res => res.data)
+            }
+        },
+        deleteCustomer: {
+            type: CustomerType,
+            args:{
+                id: {type: new GraphQLNonNull(GraphQLString)},
+            },
+            resolve(parentvalue, args){
+                return axios.delete(`/customers/${args.id}`, config)
+                    .then(res => res.data)
+            }
+        }
     }
 })
 
@@ -28,5 +94,6 @@ const RootQuery = new GraphQLObjectType({
 
 
 module.exports = new GraphQLSchema({
-
+    query: RootQuery,
+    mutation
 });
