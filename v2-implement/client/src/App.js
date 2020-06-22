f4bdc4e97caa79aef63597900f191e95b4ccb822
Nilanjan Deb <nilanjan172nsvian@gmail.com>
2020-06-22 07:22:29 +0530

    ignore client folder

---
+++ v2-implement/client/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.png';
+
+
+const App = () => {
+  return (
+    <>
+      <img 
+        src={logo} 
+        alt="graphql" 
+        style={{ 
+          width: 300, 
+          margin:'auto', 
+          display: 'block' 
+        }} 
+      />
+    </>
+  )
+}
+
+export default App
+
